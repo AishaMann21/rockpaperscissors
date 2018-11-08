57601fcbe508095d5e40383761123cff375584d9
Aisha Mann <aishamann21@gmail.com>
2018-11-08 23:25:44 +0000

    Added click handler and changed background color

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-//GLOBAL VARIABLES
-/* global $ */
-
-
-
-// DOCUMENT READY FUNCTION BELOW
+$("#shoot").click(function() {
+    $("#userChoice").text($("#input").val());
+});
 
