9f627ea3256d8df8a14a658c425feb605b6b491f
Aisha Mann <aishamann21@gmail.com>
2018-11-15 23:28:09 +0000

    added the math library

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
+var userChoice = "" ;
+var computerChoice = "" ;
+var winner = "" ;
+var randomNumber= 0 ;
+
 $("#shoot").click(function() {
     $("#userChoice").text($("#input").val());
+    randomNumber = Math.random();
+    if(randomNumber >.60) {
+        $("#computerChoice").html("rock"); 
+    } else if (randomNumber < 0.40) {
+        $("#computerChoice").html("paper"); 
+     } else {
+        $("#computerChoice").html ("scissors");
+     }
 });
 
