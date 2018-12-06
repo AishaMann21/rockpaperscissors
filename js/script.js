d59b0577159f3827b86429220825300ae13388a2
Aisha Mann <aishamann21@gmail.com>
2018-12-06 23:26:00 +0000

    Added compound statements

--- js/script.js
+++ js/script.js
@@ -4,14 +4,47 @@ var winner = "" ;
 var randomNumber= 0 ;
 
 $("#shoot").click(function() {
-    $("#userChoice").text($("#input").val());
+    userChoice = $("#input").val();
+    $("#userChoice").text(userChoice);
     randomNumber = Math.random();
-    if(randomNumber >.60) {
+    if(randomNumber <.333) {
+        computerChoice= "rock";
         $("#computerChoice").html("rock"); 
-    } else if (randomNumber < 0.40) {
+    } else if (randomNumber < 0.666) {
+        computerChoice= "paper";
         $("#computerChoice").html("paper"); 
      } else {
+        computerChoice= "scissors";
         $("#computerChoice").html ("scissors");
      }
+     
+     if (userChoice=== "rock" && computerChoice === "rock" ){
+         winner = "Uh-oh, its a tie!";
+     } else if (userChoice=== "rock" && computerChoice === "paper"){
+         winner="Computer Wins!";
+     } else if (userChoice=== "rock" && computerChoice === "scissors"){
+        winner="User Wins!";
+    }
+     
+     if (userChoice=== "paper" && computerChoice === "rock" ){
+         winner = "User Wins!";
+     } else if (userChoice=== "paper" && computerChoice === "paper"){
+         winner="Uh-oh, its a tie!";
+     } else if (userChoice=== "paper" && computerChoice === "scissors"){
+        winner="Computer Wins!";
+        }
+        
+        
+    if (userChoice=== "scissors" && computerChoice === "rock" ){
+         winner = "Computer Wins!";
+     } else if (userChoice=== "scissors" && computerChoice === "paper"){
+         winner="User Wins!";
+     } else if (userChoice=== "scissors" && computerChoice === "scissors"){
+        winner="Uh-oh, its a tie!";
+        }
+        
+        
+     
+     $("#result").html(winner);
 });
 
